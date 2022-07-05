6fe0300d3f1209232cd8559ee91de4a21b0c1bb4
Eugene <79318195+eugene-coding@users.noreply.github.com>
2022-07-05 03:05:58 +0300

    Added deletion of the center-to-center distance

---
+++ Commands/DeleteCenterDistance.cs
@@ -0,0 +1,18 @@
+﻿using CBox.Resources;
+
+namespace CBox.Commands
+{
+    public class DeleteCenterDistance : ICommand
+    {
+        public void Execute()
+        {
+            var shapeRange = Main.App.ActiveSelectionRange;
+
+            if (shapeRange.Count > 0)
+            {
+                var shapes = shapeRange.Shapes.FindShapes(Resource.CenterDistanceEllipseName);
+                shapes.Delete();
+            }
+        }
+    }
+}
