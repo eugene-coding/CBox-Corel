b1eaf09a0e605c5be3bbede67a4eba578be593dd
Eugene <79318195+eugene-coding@users.noreply.github.com>
2022-07-05 03:07:00 +0300

    Added code to update the Corel screen if the program refresh the screen

---
+++ Commands/Refresh.cs
@@ -0,0 +1,11 @@
+﻿namespace CBox.Commands
+{
+    public class Refresh : ICommand
+    {
+        public void Execute()
+        {
+            Main.App.Optimization = false;
+            Main.App.Refresh();
+        }
+    }
+}
